af2471044075e4443844f43be16f765357dcc6cd
SubirChDey <subirdey93@gmail.com>
2025-03-10 23:54:09 +0600

    redirects added for reloading

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -33,7 +33,7 @@ const WatchList = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        fetch(`http://localhost:5000/watchlist/${_id}`, {
+        fetch(`https://chill-gamer-server-sand.vercel.app/watchlist/${_id}`, {
           method: 'DELETE',
         })
           .then((res) => res.json())
